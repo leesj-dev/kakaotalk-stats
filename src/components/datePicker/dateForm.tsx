1ddaa6619a501d88cbd4fe371d7da829aa6605e5
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-22 01:45:58 +0900

    fix:conflict 수정

--- src/components/datePicker/dateForm.tsx
+++ src/components/datePicker/dateForm.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import "react-datepicker/dist/react-datepicker.css"; // react-datepicker의 CSS 파일을 import
+
 
 const DatePickerInputContainer = styled.div`
   color: #fff;
@@ -20,16 +21,34 @@ const DateForm = () => {
   const [startDate, endDate] = dateRange;
 
   // redux에서 분석된 메시지 데이터 가져오기
-  // const analyzedMessageData = useSelector(state => state.analyzedMessageData);
+  const results = useSelector((state: { analyzedMessagesSlice: AnalyzedMessages }) => state.analyzedMessagesSlice);
+console.log(results)
 
-  // useDispatch 훅을 사용하여 dispatch 함수 가져오기
-  const dispatch = useDispatch();
-
+// results 변수의 타입을 AnalyzedMessages 타입으로 정의
+type AnalyzedMessages = Array<Array<{ speaker: string, dates: Array<{ date: string, data: any }> }>>; // 예시에 사용된 타입 정의
   // 분석된 메시지 데이터 사용하기
   // 예시: 시작 날짜와 종료 날짜 출력
-  //   console.log("startDate:", analyzedMessageData.startDate);
-  //   console.log("endDate:", analyzedMessageData.endDate);
+//1
+// if (results && results.length > 0 && results[0][0] && results[0][0].dates && results[0][0].dates.length > 0) {
+//     console.log("startDate:", results[0][1].dates[0].date);
+//     console.log("endDate:", results[0][1].dates.slice(-1)[0].date);
+//   } else {
+//     console.log("Some values are undefined or empty.");
+//   }
+//2
+interface DateData {
+    date: string;
+    data: any; // data 필드의 타입은 필요에 따라 수정하세요
+  }
+// 모든 대화자의 날짜 배열을 합치기
+const allDates = ([] as DateData[]).concat(...results.map(result => result[0].dates));
 
+// 가장 이른 날짜와 가장 늦은 날짜 찾기
+const speakerStartDate = allDates.reduce((prev, curr) => (prev.date < curr.date ? prev : curr));
+const speakerEndDate = allDates.reduce((prev, curr) => (prev.date > curr.date ? prev : curr));
+
+console.log("처음 대화 날짜:", speakerStartDate.date);
+console.log("마지막 대화 날짜:", speakerEndDate.date);
   return (
     <div>
       <DatePickerInputContainer>
