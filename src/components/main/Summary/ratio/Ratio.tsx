58ecb4d73c67c8433d6cfe0097c222da7bf1286d
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-04-28 12:41:00 +0900

    feat:pieGraph 각 채팅방 선택시 선택한 채팅방의 대화자 대화수 그래프에 적용

--- src/components/main/Summary/ratio/Ratio.tsx
+++ src/components/main/Summary/ratio/Ratio.tsx
@@ -1,44 +1,61 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
 import { AnalyzedMessage } from "../../../../@types/index.d";
-import { getChatTimes, getSpeakers } from "../../../../module/common/getProperties";
 
 const COLORS = ["#FF414D", "#FF8991", "#F7ABB1"];
 
 const Ratio = () => {
-  const results = useSelector((state: { analyzedMessagesSlice: AnalyzedMessage[] }) => state.analyzedMessagesSlice);
-  const speakers = getSpeakers(results);
-  const chatTimes = getChatTimes(results);
-  // 현재 채팅방 총 대화 수
+  const results = useSelector(
+    (state: { analyzedMessagesSlice: AnalyzedMessage[] }) =>
+      state.analyzedMessagesSlice
+  );
+  const selectedChatRoomIndex = useSelector(
+    (state: { selectedRoomIndexSlice: number }) => state.selectedRoomIndexSlice
+  );
+  // 현재 채팅방 대화자 한명 대화수
 
-  const totalConversation = (chatTimes: any) => {
-    let chatTimesMergeNewObj = chatTimes.map((arr: any) => {
-      return arr.reduce((acc: any, obj: any) => {
-        return { ...acc, ...obj };
-      }, {});
-    });
+  const selectedChatRoomData = results[selectedChatRoomIndex];
+  const speakerTotalChatCounts: Record<string, number> = {};
+  Object.values(selectedChatRoomData).forEach((chatroom) => {
+    Object.values(chatroom).forEach(
+      (chat: { chatTimes: any; speaker: string }) => {
+        const speaker = chat.speaker;
+        if (!speakerTotalChatCounts[speaker]) {
+          speakerTotalChatCounts[speaker] = 0;
+        }
+        const chatTimes = chat.chatTimes;
+        const chatCounts = chatTimes ? Object.values(chatTimes) : [];
+        const totalChatCount = chatCounts.reduce(
+          (acc, count) => Number(acc) + Number(count),
+          0
+        );
+        speakerTotalChatCounts[speaker] += Number(totalChatCount);
+      }
+    );
+  });
 
-    const chatsArray = Object.values(chatTimesMergeNewObj[0]);
-    let sum = 0;
-    chatsArray.forEach((chat: any) => {
-      sum += chat;
-    });
-    return sum;
-  };
-  const totalSum = totalConversation(chatTimes);
-  // useEffect(() => {
-  //   console.log(chatTimes, "speakersspeakersspeakers");
-  // }, [results]);
-  let data = [
-    { name: speakers[0][0], value: 400 },
-    { name: speakers[0][1], value: 300 },
-  ];
+  const totalChatCount = Object.values(speakerTotalChatCounts).reduce(
+    (a: number, b: number) => a + b
+  );
+  const data = Object.entries(speakerTotalChatCounts).map(([name, value]) => ({
+    name,
+    value: Number(((value / totalChatCount) * 100).toFixed(0)),
+  }));
 
   return (
     <PieChart width={400} height={400}>
-      <Pie data={data} cx={200} cy={200} innerRadius={0} outerRadius={100} dataKey="value" labelLine label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
-        {data.map((entry, index) => (
+      <Pie
+        data={data}
+        cx={200}
+        cy={200}
+        innerRadius={0}
+        outerRadius={100}
+        dataKey="value"
+        labelLine
+        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+      >
+        {data.map((entry: any, index: number) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
       </Pie>
