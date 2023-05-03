a35576159f477d2e81d4bc40e05342973a64f87d
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-05-03 23:37:19 +0900

    feat:각각의 대화자 일별 대화 수 반환

--- src/components/datePicker/dateForm.tsx
+++ src/components/datePicker/dateForm.tsx
@@ -64,20 +64,16 @@ const DateForm = () => {
 
   useEffect(() => {
     const start = dateAll[0];
-    const end = dateAll[dateAll.length - 1];
+    const end = dateAll.at(-1);
     if (start && end !== undefined) {
       setStartDateSpeaker(start.match(/.{1,2}/g));
       setEndDateSpeaker(end.match(/.{1,2}/g));
-    } else {
-      // console.log("입력된 문자열이 정의되어 있지 않습니다.");
     }
-    setStartDateSpeaker(start);
-    setEndDateSpeaker(end);
     // *****
     // 컴포넌트 렌더링을 시키기 위해서 dateAll을 의존성배열에 추가
     // dateAll은 useSelector로 불러온 result값이 갱신되면 업데이트 되고
     // 업데이트 되면서 useEffect가 실행이 된다.
-  }, [dateAll]); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정
+  }, [setLimitTime]); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정
 
   // ************
   // dispatch(
@@ -85,6 +81,7 @@ const DateForm = () => {
   // );
   // 예상) ""을 할당함  => useEffect로 업데이트 된 값을 할당함 => 재렌더링 함 => ""을 할당함,useEffect로 업데이트 된 값을 할당함,재렌더링 함
   // 따라서, useEffect로 업데이트 된 값을 할당한 뒤에 => dispatch 하도록 변경함.
+
   useEffect(() => {
     dispatch(
       setLimitTime([{ startDate, endDate, startDateSpeaker, endDateSpeaker }])
@@ -106,17 +103,18 @@ const DateForm = () => {
             // dateRange 상태 변수를 업데이트
             onChange={(update: any, event: any) => {
               event.preventDefault();
+
               event.persist();
               setDateRange(update as null[]);
             }}
             minDate={
               new Date(
-                `20${startDateSpeaker[0]}, ${startDateSpeaker[1]}, ${startDateSpeaker[2]}`
+                `20${startDateSpeaker[0]}. ${startDateSpeaker[1]}. ${startDateSpeaker[2]}.`
               )
             } // 현재 날짜 이전의 날짜는 선택할 수 없도록 설정
             maxDate={
               new Date(
-                `20${endDateSpeaker[0]}, ${endDateSpeaker[1]}, ${endDateSpeaker[2]}`
+                `20${endDateSpeaker[0]}. ${endDateSpeaker[1]}. ${endDateSpeaker[2]}.`
               )
             } // 현재 날짜 이후의 날짜는 선택할 수 없도록 설정
             // withPortal 프로퍼티를 true로 설정하면 달력이 렌더링되는 위치를 설정,기본값은 false
