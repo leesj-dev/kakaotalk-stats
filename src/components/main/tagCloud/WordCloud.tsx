954e18124ca1c615d86786d27daa5cb199d0b2d1
youngentry <gentry_@naver.com>
2023-05-03 19:07:28 +0900

    feat(Keyword): 특정 키워드 제외하기, 웃음 제외하기 기능 추가

--- src/components/main/tagCloud/WordCloud.tsx
+++ src/components/main/tagCloud/WordCloud.tsx
@@ -16,6 +16,109 @@ const KeywordList = styled.li`
   background: #f3f0ff;
 `;
 
+/**
+ * 모든 키워드에서 상위 N개의 키워드를 가져옵니다.
+ * @param {KeywordCounts} allKeywords - 모든 키워드의 카운트입니다.
+ * @param {number} n - 가져올 상위 키워드의 수입니다.
+ * @returns {ValueCountPair[]} 상위 N개의 키워드입니다.
+ */
+const getAllTopNKeywords = (allKeywords: KeywordCounts, n: number) => {
+  const keywordsEntries: ValueCountPair[] = Object.entries(allKeywords).map(([value, count]) => ({
+    value,
+    count,
+  }));
+  const sortedKeywordsEntries: ValueCountPair[] = keywordsEntries.sort(
+    (a: ValueCountPair, b: ValueCountPair) => b.count - a.count
+  );
+  const topNKeywords: ValueCountPair[] = sortedKeywordsEntries.slice(0, n + 1);
+  return topNKeywords;
+};
+
+/**
+ * 각 speaker의 키워드 배열에서 상위 N개의 키워드를 가져옵니다.
+ * @param {KeywordCounts[]} keywordsArray - speaker 키워드 배열입니다.
+ * @returns {ValueCountPair[]} 상위 N개의 키워드입니다.
+ */
+const getSpeakersTopNKeywords = (keywordsArray: KeywordCounts[], displayKeywordCount: number) => {
+  const allKeywords: KeywordCounts = {};
+  keywordsArray.forEach((keywords: KeywordCounts) => {
+    for (const key in keywords) {
+      allKeywords[key] ? (allKeywords[key] += keywords[key]) : (allKeywords[key] = keywords[key]);
+    }
+  });
+
+  const topNKeywords: ValueCountPair[] = getAllTopNKeywords(allKeywords, displayKeywordCount);
+  return topNKeywords;
+};
+
+/**
+ * 현재 키워드 카운트 배열에서 speaker별로 상위 키워드를 가져옵니다.
+ * @param {KeywordCounts[][]} currentKeywordCounts - 현재 키워드 카운트 배열입니다.
+ * @returns {ValueCountPair[][]} speaker별로 상위 키워드입니다.
+ */
+const getHighKeywords = (
+  currentKeywordCounts: KeywordCounts[][],
+  displayKeywordCount: number,
+  keywordsToFilter: string[],
+  isLaughterFiltered: boolean
+) => {
+  const highKeywords: ValueCountPair[][] = [];
+  for (const keywordsArray of currentKeywordCounts) {
+    highKeywords.push(getSpeakersTopNKeywords(keywordsArray, displayKeywordCount));
+  }
+  const filteredKeyword = filterSpecificKeywords(highKeywords, keywordsToFilter);
+  console.log(filteredKeyword, "filteredKeyword");
+  const laughterFilteredKeyword = filterLaughterKeywords(filteredKeyword);
+  return isLaughterFiltered ? laughterFilteredKeyword : filteredKeyword;
+};
+
+/**
+ * 상위 키워드에서 특정 키워드를 필터링합니다.
+ * @param {ValueCountPair[][]} highKeywords - 상위 키워드 배열입니다.
+ * @param {string[]} keywordsToFilter - 필터링할 키워드 배열입니다.
+ * @returns {ValueCountPair[][]} 필터링된 상위 키워드 배열입니다.
+ */
+const filterSpecificKeywords = (highKeywords: ValueCountPair[][], keywordsToFilter: string[]) => {
+  console.log(highKeywords);
+  const filteredKeyword = highKeywords.map((keywordArray: ValueCountPair[]) =>
+    keywordArray.filter((keyword: ValueCountPair) => !keywordsToFilter.includes(keyword.value))
+  );
+  return filteredKeyword;
+};
+
+/**
+ * 상위 키워드에서 "ㅋ" 또는 "ㅎ"를 포함하는 키워드를 필터링합니다.
+ * @param {ValueCountPair[][]} highKeywords - 상위 키워드 배열입니다.
+ * @returns {ValueCountPair[][]} 필터링된 상위 키워드 배열입니다.
+ */
+const filterLaughterKeywords = (highKeywords: ValueCountPair[][]) => {
+  console.log(highKeywords);
+  const filteredKeyword = highKeywords.map((keywordArray: ValueCountPair[]) =>
+    keywordArray.filter((keyword: ValueCountPair) => !keyword.value.includes("ㅋ" || "ㅎ"))
+  );
+  return filteredKeyword;
+};
+
+/**
+ * 키워드 데이터에서 중복되는 키워드를 가져옵니다.
+ * @param {any[]} keywordData - 키워드 데이터 배열입니다.
+ * @returns {string[]} 중복되는 키워드 배열입니다.
+ */
+const getOverlappedKeyword = (keywordData: any[]) => {
+  const overlappedKeyword: any = {};
+  keywordData.forEach((keywords) => {
+    keywords.forEach((keyword: any) => {
+      overlappedKeyword[keyword.value] = Number(overlappedKeyword[keyword.value] || 0) + 1;
+    });
+  });
+  const filteredKeyword = [];
+  for (const keyword in overlappedKeyword) {
+    overlappedKeyword[keyword] === 2 && filteredKeyword.push(keyword);
+  }
+  filteredKeyword.shift();
+  return filteredKeyword;
+};
+
 const WordCloud = () => {
   const analyzedMessages = useSelector(
     (state: { analyzedMessagesSlice: AnalyzedMessage[] }) => state.analyzedMessagesSlice
@@ -23,46 +126,23 @@ const WordCloud = () => {
   const selectedRoomIndex = useSelector(
     (state: { selectedRoomIndexSlice: number }) => state.selectedRoomIndexSlice
   );
-
   const [numberInput, setNumberInput] = useState<number>(50);
   const [displayKeywordCount, setDisplayKeywordCount] = useState<number>(50);
+  const [keywordsToFilter, setKeywordsToFilter] = useState<string[]>([]);
+  const [isLaughterFiltered, setIsLaughterFiltered] = useState<boolean>(false);
+
+  const speaker: string[] = getSpeakers(analyzedMessages)[selectedRoomIndex];
 
   const keywordCounts: KeywordCounts[][][] = getKeywordCounts(analyzedMessages);
   const currentKeywordCounts: KeywordCounts[][] = keywordCounts[selectedRoomIndex];
+  const keywordData: ValueCountPair[][] = getHighKeywords(
+    currentKeywordCounts,
+    displayKeywordCount,
+    keywordsToFilter,
+    isLaughterFiltered
+  );
 
-  const getTopNKeywords = (allKeywords: KeywordCounts, n: number) => {
-    const keywordsEntries: ValueCountPair[] = Object.entries(allKeywords).map(([value, count]) => ({
-      value,
-      count,
-    }));
-    const sortedKeywordsEntries: ValueCountPair[] = keywordsEntries.sort(
-      (a: ValueCountPair, b: ValueCountPair) => b.count - a.count
-    );
-    const topNKeywords: ValueCountPair[] = sortedKeywordsEntries.slice(0, n + 1);
-    return topNKeywords;
-  };
-
-  const getTopNKeywordsFromSpeaker = (keywordsArray: KeywordCounts[]) => {
-    const allKeywords: KeywordCounts = {};
-    keywordsArray.forEach((keywords: KeywordCounts) => {
-      for (const key in keywords) {
-        allKeywords[key] ? (allKeywords[key] += keywords[key]) : (allKeywords[key] = keywords[key]);
-      }
-    });
-    const topNKeywords: ValueCountPair[] = getTopNKeywords(allKeywords, displayKeywordCount);
-    return topNKeywords;
-  };
-
-  const getHighKeywords = (currentKeywordCounts: KeywordCounts[][]) => {
-    const highKeywords: ValueCountPair[][] = [];
-    for (const keywordsArray of currentKeywordCounts) {
-      highKeywords.push(getTopNKeywordsFromSpeaker(keywordsArray));
-    }
-    return highKeywords;
-  };
-  const keywordData: ValueCountPair[][] = getHighKeywords(currentKeywordCounts);
-
-  const speaker: string[] = getSpeakers(analyzedMessages)[selectedRoomIndex];
+  const overlappedKeyword = getOverlappedKeyword(keywordData);
 
   const handleChangeNumberInput = (e: ChangeEvent<HTMLInputElement>) => {
     setNumberInput(Number(e.target.value));
@@ -73,27 +153,17 @@ const WordCloud = () => {
     setDisplayKeywordCount(numberInput);
   };
 
-  // 키워드 중에서 겹치는 말버릇 모아보기
-  const getOverlappedKeyword = (keywordData: any[]) => {
-    const overlappedKeyword: any = {};
-    keywordData.forEach((keywords) => {
-      keywords.forEach((keyword: any) => {
-        console.log(keyword.value, "overlappedKeyword[keyword.value]");
-        overlappedKeyword[keyword.value] = Number(overlappedKeyword[keyword.value] || 0) + 1;
-      });
-    });
-    const filteredKeyword = [];
-    for (const keyword in overlappedKeyword) {
-      overlappedKeyword[keyword] === 2 && filteredKeyword.push(keyword);
-    }
-    filteredKeyword.shift();
-    return filteredKeyword;
+  const handClickExceptEmoticon = () => {
+    setKeywordsToFilter([...keywordsToFilter, "이모티콘", "사진", "동영상"]);
   };
-  const overlappedKeyword = getOverlappedKeyword(keywordData);
-  console.log(overlappedKeyword);
+  const handClickExceptLaughter = () => {
+    setIsLaughterFiltered(!isLaughterFiltered);
+  };
 
   return (
     <ul>
+      <div onClick={handClickExceptEmoticon}>이모티콘,사진,동영상 제외하기</div>
+      <div onClick={handClickExceptLaughter}>ㅋ,ㅎ 제외하기</div>
       <form action="" onSubmit={handleSubmitNumber}>
         <label>내 카톡 습관, 몇 개나 모아서 볼래?</label>
         <input name="" type="number" id="" value={numberInput} onChange={handleChangeNumberInput} />
@@ -101,7 +171,6 @@ const WordCloud = () => {
       </form>
       {keywordData.length &&
         keywordData.map((data: ValueCountPair[], index: number) => {
-          console.log(keywordData);
           return (
             <KeywordList key={index}>
               {speaker[index]}
