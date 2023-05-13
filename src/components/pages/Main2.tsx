128529e30bb413e810b7cc3d264b96ef5575b62c
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-05-13 14:14:41 +0900

    fix:templates 삭제 -> pages에 올가니즘 삽입

--- src/components/pages/Main2.tsx
+++ src/components/pages/Main2.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import IntroWithFunctionCard from "../templates/IntroWithFunctionCard";
+import React, { useRef } from "react";
+import FunctionCard from "../organisms/FunctionCard";
+import MainVisual from "../organisms/MainVisual";
+import styled from "styled-components";
+import Ftn from "../atoms/Ftn";
 
-const Main = () => {
+const Main2Container = styled.div`
+  width: 100%;
+`;
+
+const Main2Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Main2 = () => {
+  // const moveTarget = useRef();
+  // const onMoveToFunctionCard = () => {
+  //   moveTarget.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  // };
   return (
-    <div>
-      <IntroWithFunctionCard />
-    </div>
+    <Main2Container>
+      <Main2Wrapper>
+        <Ftn />
+        <MainVisual
+        //onMoveToFunctionCard={onMoveToFunctionCard}
+        />
+        <FunctionCard
+        // ref={moveTarget}
+        />
+      </Main2Wrapper>
+    </Main2Container>
   );
 };
 
-export default Main;
+export default Main2;
