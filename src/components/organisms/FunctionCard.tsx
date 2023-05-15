e3bfbc317b1c3c4cc6557d843f79a4865d9c84c7
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-05-15 15:56:15 +0900

    fix:충돌해결

--- src/components/organisms/FunctionCard.tsx
+++ src/components/organisms/FunctionCard.tsx
@@ -63,6 +63,7 @@ const SideSlide = styled.div`
   top: 0;
   left: 0;
   transition: transform 0.5s ease-in-out;
+  border-right: 1px solid ${(props) => props.theme.mainGray};
   > * {
     height: 100%;
     width: 100%;
