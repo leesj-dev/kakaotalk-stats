e410602d5d72c0b639b86f7f313406594fa64713
youngentry <gentry_@naver.com>
2023-06-15 01:26:02 +0900

    refactor: 불필요한 파일 삭제, 폴더 정리

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./style/Theme";
 import MainPage from "./components/pages/MainPage";
 import { Routes, Route } from "react-router-dom";
+import NavHead from "./components/sections/NavHead";
 import AttachmentPage from "./components/pages/AttachmentPage";
 import DashboardPage from "./components/pages/DashboardPage";
 import Footer from "./components/sections/Footer";
@@ -11,10 +12,6 @@ import { useSelector } from "react-redux";
 import FloatingMenu from "./components/organisms/FloatingMenu";
 import Wrapper from "./components/wrapper/Wrapper";
 import GlobalStyle from "./style/GlobalStyles";
-<<<<<<< HEAD
-import NavHead from "./components/sections/NavHead";
-=======
->>>>>>> 5bd5030 (feat: defaultTheme 생성 여기저기 css 달라짐)
 
 function App() {
   const isDarkMode = useSelector((state: { isDarkModeSlice: boolean }) => state.isDarkModeSlice);
--- src/style/GlobalStyles.tsx
+++ src/style/GlobalStyles.tsx
@@ -1,6 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 
+import PretendardLight from "../fonts/PretendardLight.woff2";
+import PretendardRegular from "../fonts/PretendardRegular.woff2";
+import PretendardMedium from "../fonts/PretendardMedium.woff2";
+import PretendardBold from "../fonts/PretendardBold.woff2";
+
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "Pretendard";
+    src: local('PretendardLight'), local('PretendardLight'), url(${PretendardLight}) format('woff2');
+    font-weight:300;
+  }
+  @font-face {
+    font-family: "Pretendard";
+    src: local('PretendardRegular'), local('PretendardRegular'), url(${PretendardRegular}) format('woff2');
+    font-weight:400;
+  }
+  @font-face {
+    font-family: "Pretendard";
+    src: local('PretendardMedium'), local('PretendardMedium'), url(${PretendardMedium}) format('woff2');
+    font-weight:500;
+  }
+  @font-face {
+    font-family: "Pretendard";
+    src: local('PretendardBold'), local('PretendardBold'), url(${PretendardBold}) format('woff2');
+    font-weight:700;
+  }
+
 
 html {
   box-sizing: border-box;
@@ -23,6 +50,8 @@ html {
 
 body {
 
+  font-family: "Pretendard";
+  font-weight: 900;
   margin: 0;
   padding: 0;
   font-family: "Pretendard";
@@ -81,10 +110,7 @@ input,
 textarea,
 button {
   font-size: inherit;
-<<<<<<< HEAD
-=======
   font-family: "Noto Sans KR";
->>>>>>> 5bd5030 (feat: defaultTheme 생성 여기저기 css 달라짐)
   font-weight: inherit;
   margin: 0;
 }
@@ -104,7 +130,7 @@ address,
 em,
 i {
   font-style: normal;
-
+  font-weight:100
 }
 
 hr {
@@ -128,9 +154,6 @@ button,
 input,
 select,
 textarea {
-
-  font-family: inherit;
-
   font-size: inherit;
   font-weight: inherit;
   margin: 0;
