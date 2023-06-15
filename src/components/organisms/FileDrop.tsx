defcbf7b1265d078d5be27d6de08e88ab0a5b501
youngentry <gentry_@naver.com>
2023-06-15 18:56:55 +0900

    feat: 드래그앤 드롭

--- src/components/organisms/FileDrop.tsx
+++ src/components/organisms/FileDrop.tsx
@@ -6,7 +6,6 @@ import Span from "../atoms/Span";
 import OsList from "./OsList";
 import { useDispatch } from "react-redux";
 import { pushNewlyAttachedFiles } from "../../store/reducer/attachedFileListSlice";
-import { VscNewFile } from "react-icons/vsc";
 import { FlexColumnCenterDiv } from "../styleComponents/FlexDiv";
 
 const DropBox = styled(FlexColumnCenterDiv)`
@@ -16,12 +15,10 @@ const DropBox = styled(FlexColumnCenterDiv)`
   margin: 0 auto;
   border: 3px dashed ${(props) => props.theme.mainGray};
   border-radius: 30px;
-  z-index: 2;
 
   > * {
     margin-bottom: 10px;
     font-weight: 300;
-    text-align: center;
   }
   > :nth-child(1) {
     margin-bottom: 3rem;
@@ -41,10 +38,15 @@ const AttachmentBox = styled.div`
   gap: 5px;
 `;
 
+const TextContentBox = styled(FlexColumnCenterDiv)``;
+
 const DragFile = styled(FlexColumnCenterDiv)`
+  /* position: absolute; */
+  display: flex;
   width: 100%;
+  height: 100%;
   gap: 20px;
-  z-index: 1;
+  background: #f00;
 `;
 
 type DropZoneProps = {
@@ -52,38 +54,6 @@ type DropZoneProps = {
 };
 
 const FileDrop = ({ handleChangeFile }: DropZoneProps) => {
-  const dispatch = useDispatch();
-
-  const [dragging, setDragging] = useState(false);
-
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(true);
-  };
-
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(false);
-  };
-
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
-
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(false);
-
-    const files: any = Array.prototype.slice.call(e.dataTransfer.files);
-    if (files && files.length) {
-      dispatch(pushNewlyAttachedFiles(files));
-    }
-
-    // const fileArray: any = Array.prototype.slice.call(files);
-    // pushNewlyAttachedFiles(fileArray);
-    // 파일 처리 로직을 수행합니다.
-  };
-
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
 
   const handleResize = () => {
@@ -97,62 +67,26 @@ const FileDrop = ({ handleChangeFile }: DropZoneProps) => {
     };
   }, []);
 
-  const handleIconDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(true);
-  };
-
-  const handleIconDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(true);
-  };
-
-  const handleIconDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragging(true);
-  };
-
   return (
-    <DropBox
-      className={`drop-zone ${dragging ? "dragging" : ""}`}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-    >
-      {dragging ? (
-        <DragFile
-          onDragEnter={handleIconDragEnter}
-          onDragLeave={handleIconDragLeave}
-          onDragOver={handleIconDragOver}
-        >
-          <VscNewFile size={60} />
-          <Span fontSize="20px">Drop Files Here</Span>
-        </DragFile>
+    <DropBox>
+      <OsList />
+      {screenWidth > 769 ? (
+        <TextContentBox>
+          <Span fontSize="18px">내보내기한 카카오톡 텍스트 파일을 드래그하여 끌어 놓거나</Span>
+          <AttachmentBox>
+            <AttachmentButton onChange={handleChangeFile}>첨부하기</AttachmentButton>
+            <Paragraph fontSize="18px">버튼을 클릭하세요.</Paragraph>
+          </AttachmentBox>
+        </TextContentBox>
       ) : (
-        <>
-          <OsList />
-          {screenWidth > 769 ? (
-            <>
-              <Span fontSize="18px">
-                내보내기한 카카오톡 텍스트 파일을 드래그하여 여기에 끌어 놓거나
-              </Span>
-              <AttachmentBox>
-                <AttachmentButton onChange={handleChangeFile}>첨부하기</AttachmentButton>
-                <Paragraph fontSize="18px">버튼을 클릭하세요.</Paragraph>
-              </AttachmentBox>
-            </>
-          ) : (
-            <>
-              <Span fontSize="18px">카카오톡 텍스트 파일</Span>
-              <AttachmentBox>
-                <AttachmentButton onChange={handleChangeFile}>첨부하기</AttachmentButton>
-              </AttachmentBox>
-            </>
-          )}
-          <Span fontSize="15px">* 올바른 운영체제를 선택하지 않으면 분석이 불가능합니다.</Span>
-        </>
+        <TextContentBox>
+          <Span fontSize="18px">카카오톡 텍스트 파일</Span>
+          <AttachmentBox>
+            <AttachmentButton onChange={handleChangeFile}>첨부하기</AttachmentButton>
+          </AttachmentBox>
+        </TextContentBox>
       )}
+      <Span fontSize="15px">* 올바른 운영체제를 선택하지 않으면 분석이 불가능합니다.</Span>
     </DropBox>
   );
 };
