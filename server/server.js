93842f0bd01fc917985f3b88ee0c47f1f8bc144f
youngentry <gentry_@naver.com>
2023-07-18 21:53:03 +0900

    chore: 해쉬화 모듈 bcrypt 설치 및 회원가입 password 해쉬화 적용

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config({ path: "../.env" });
+const { hashPassword } = require("./module/hashPassword/hashPassword");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -35,29 +36,31 @@ const userSchema = new mongoose.Schema({
 // user model을  class로 생성합니다.
 const User = mongoose.model("User", userSchema);
 
-// user를 DB에 저장합니다.
-app.post("/api/users/create", (req, res) => {
-  const { userId, password, nickname } = req.body;
-  const newUser = new User({ userId, password, nickname });
+// 회원 가입 핸들러
+app.post("/api/users/create", async (req, res) => {
+  try {
+    const { userId, password, nickname } = req.body;
+    const hashedPassword = await hashPassword(password);
 
-  newUser
-    .save()
-    .then(() => {
-      // 회원 가입 성공
-      res.status(200).json({ message: "회원 가입이 완료되었습니다." });
-    })
-    .catch((err) => {
+    const newUser = new User({ userId, password: hashedPassword, nickname });
+
+    await newUser.save();
+
+    // 회원 가입 성공
+    res.status(200).json({ message: "회원 가입이 완료되었습니다." });
+  } catch (error) {
+    if (error.code === 11000) {
       // 중복된 아이디나 닉네임인 경우 처리
-      if (err.code === 11000) {
-        const key = Object.keys(err.keyValue)[0];
-        const value = err.keyValue[key];
-        console.error(err, `error[11000]: 중복된 아이디나 닉네임 에러`);
-        res.status(409).json({ error: `${key} '${value}'는 이미 사용 중입니다.` });
-      } else {
-        // 기타 오류 처리
-        res.status(500).json({ error: "회원 가입 중 오류가 발생했습니다." });
-      }
-    });
+      const key = Object.keys(error.keyValue)[0];
+      const value = error.keyValue[key];
+      console.error(error, `error[11000]: 중복된 아이디나 닉네임 에러`);
+      res.status(409).json({ error: `${key} '${value}'는 이미 사용 중입니다.` });
+    } else {
+      // 기타 오류 처리
+      console.error(error);
+      res.status(500).json({ error: "회원 가입 중 오류가 발생했습니다." });
+    }
+  }
 });
 
 app.get("/", (req, res) => {
