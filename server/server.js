dfa794ede11923d4626c9811c4d37c74127815a2
youngentry <gentry_@naver.com>
2023-07-18 21:39:52 +0900

    chore: dotenv 패키지 설치 및 환경변수 설정

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config({ path: "../.env" });
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGODB_CONNECTION;
 
 // MongoDB 연결 설정
 mongoose
